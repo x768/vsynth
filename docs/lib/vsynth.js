a09d5e5cbb4a18950304dfe3c347864c38ad0f8c
x768 <x@x768.com>
2026-09-17 22:56:13 +0900

    Fix: alveolar plosive

--- docs/lib/vsynth.js
+++ docs/lib/vsynth.js
@@ -344,7 +344,7 @@ class ResonatorSet
                 amp = 0.15;
             } else if (prm.f > 1) {
                 pitch = (prm.f + 5) * 450;
-                amp = (3 - prm.f) * 0.05;
+                amp = (5 - prm.f) * 0.025;
             } else {
                 pitch = (prm.f + 2) * 900;
                 amp = 0.10;
